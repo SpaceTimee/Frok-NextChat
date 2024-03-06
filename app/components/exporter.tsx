a4bfbcba3d80598146d18bb477a6d3fd9a9fe37c
Space Time <55327009+SpaceTimee@users.noreply.github.com>
2024-03-06 13:40:36 +0800

    change title

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -544,9 +544,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>Frok NextChat</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              github.com/SpaceTimee/Frok-NextChat
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
